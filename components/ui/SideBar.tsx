221106c6da09b9bf817d2bbcb79f7ba6effffa1c
redhairshanks362 <142970205+redhairshanks362@users.noreply.github.com>
2024-10-20 20:24:04 +0530

    Implemented sidebar with logo and label effect
    Check comments

--- components/ui/SideBar.tsx
+++ components/ui/SideBar.tsx
@@ -16,12 +16,13 @@ const SideBar = ({user} :SiderbarProps) => {
             <Image src="/icons/logo.svg" width={34} height={34} alt='Horizon logo' className='size-[24px] max-xl:size-14'/>
             <h1 className='sidebar-logo'> Horizon</h1>
             </Link>
-            // links to pointing to other pages 
-            // we are using className here where it will highlight the active tab in sidebar for that we will make use of cn 
-            // it is sort of like ngClass where we apply custom tailwind based on boolean flag or conditions
+            //links to pointing to other pages 
+            //we are using className here where it will highlight the active tab in sidebar for that we will make use of cn 
+            //it is sort of like ngClass where we apply custom tailwind based on boolean flag or conditions
             {sidebarLinks.map((item) => {
               const isActive = pathName === item.route || pathName.startsWith(`${item.route}/`)
-              return (<Link href={item.route} key={item.label} className={cn('sidebar-link', {'bg-bank-gradient': isActive})}> {item.label} </Link>)
+              //instead of returning item label we will return div
+              return (<Link href={item.route} key={item.label} className={cn('sidebar-link', {'bg-bank-gradient': isActive})}> <div className='relative size-6'><Image src={item.imgURL} alt={item.label} fill className={cn({'brightness-[3] invert-0' : isActive})}></Image></div> </Link>)
             })}
         </nav>
     </section>
